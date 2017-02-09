4a27fe9d32edeabd7a57b7946485fe3493f5a199
Nikolas Stott <nikolas.stott@hotmail.fr>
2017-02-09 16:25:39 +0100

    added camera for boids

--- boids/src/Boid.cpp
+++ boids/src/Boid.cpp
@@ -12,9 +12,5 @@ Boid::~Boid() {
 
 void Boid::draw()
 {
-	glPointSize(20.0);
-        glBegin(GL_POINTS);
-        glColor3f(1.0,1.0,0.0);
-	glVertex3(0.0,0.0,0.0);
-        glEnd();
+	glutSolidCube(1);
 }
--- boids/src/app_boids.cpp
+++ boids/src/app_boids.cpp
@@ -5,16 +5,28 @@
 #include <math.h>
 #include <time.h>
 #include <vector>
+#include "camera_trackball.h"
 #include "Boid.h"
 
 using namespace std;
 
-int FPS = 30;
-int WINDOW_X = 600;
-int WINDOW_Y = 600;
-int WINDOW_WIDTH = 600;
-int WINDOW_HEIGHT = 600;
 
+const float FOVY = 60.0f;
+const float NEARCLIP = 0.1f;
+const float FARCLIP = 100.0f;
+const int FPS = 30;
+const int WINDOW_X = 700;
+const int WINDOW_Y = 100;
+// Inputs
+int mouse_x = 0;
+int mouse_y = 0;
+int mouse_buttons[GLUT_NUM_MOUSE_BUTTONS];
+int window_w = 800;
+int window_h = 600;
+// Camera
+CameraTrackball camera;
+
+// Time management
 float t = 0;
 float last_t = 0;
 float dt = 0;
@@ -29,19 +41,25 @@ void init(void)
 	glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_NORMALIZE);
 	glShadeModel(GL_SMOOTH);
-
+  
+	// Init inputs
+	for (int i(0); i<GLUT_NUM_MOUSE_BUTTONS; ++i) {mouse_buttons[i] = GLUT_UP;}
+  
+	// Init camera
+	camera.init({0.0f,0.0f,0.0f}, 10.0f);
+	
+	// Boid initialisation
 	b = Boid();
 }
 
 
 void display() 
 {
-        //Camera setup
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0, -1, 1,0, 0, 0, 0.0, 0.0, 1.0);
-
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	//Camera setup
+	camera.lookAt();
+    
     //Display functions
 	b.draw();
 
@@ -54,9 +72,7 @@ void reshape(int w, int h)
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60, (GLfloat)w / (GLfloat)h, 1.0, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+	gluPerspective(FOVY, (GLfloat)w / (GLfloat)h, NEARCLIP, FARCLIP);
 }
 
 
@@ -74,10 +90,31 @@ void systemEvolution()
 }
 
 
-void timer(int v) 
-{
-	glutPostRedisplay();
-	glutTimerFunc(1000 / FPS, timer, 0);
+void mouseButton(int button, int state, int x, int y) {
+  mouse_buttons[button] = state;
+  
+  // Update camera
+  camera.zoom(mouse_buttons[3] - mouse_buttons[4]);
+}
+
+void mousePassiveMotion(int x, int y) {
+  mouse_x = x; mouse_y = y;
+}
+
+void mouseMotion(int x, int y) {
+  int mouse_dx = mouse_x - x; int mouse_dy = mouse_y - y;
+  float dxn = static_cast<float>(mouse_dx) / static_cast<float>(window_w);
+  float dyn = -static_cast<float>(mouse_dy) / static_cast<float>(window_h);
+  mouse_x = x; mouse_y = y; 
+
+  // Update camera
+  if (mouse_buttons[GLUT_LEFT_BUTTON] == GLUT_DOWN)  camera.rotate(dxn, dyn);
+  if (mouse_buttons[GLUT_RIGHT_BUTTON] == GLUT_DOWN) camera.pan(dxn, dyn, 0.0f);
+}
+
+void timer(int v) {
+  glutPostRedisplay();
+  glutTimerFunc(1000 / FPS, timer, 0);
 }
 
 
@@ -86,18 +123,19 @@ int main(int argc, char** argv)
 {
 	
 	glutInit(&argc, argv);
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutInitWindowSize(window_w, window_h);
 	glutInitWindowPosition(WINDOW_X, WINDOW_Y);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	glViewport(0, 0, window_w, window_h);
 	glutCreateWindow("Test window");
 
 	init();
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
-	//glutMouseFunc();
+	glutMouseFunc(mouseButton);
+	glutPassiveMotionFunc(mousePassiveMotion);
 	glutKeyboardFunc(processKeys);
-	//glutMotionFunc();
+	glutMotionFunc(mouseMotion);
 
 
 	glutIdleFunc(systemEvolution);
